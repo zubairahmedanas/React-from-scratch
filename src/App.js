6334e208c9a2cdcb68ae7e9326d136610b93d7e8
zubairahmedanas <amianasahmed@gmail.com>
2021-10-02 02:16:43 +0600

    Zubair | css stylesheet added

--- src/App.js
+++ src/App.js
@@ -13,10 +13,14 @@ import EventBind from './components/EventBind';
 import ParentComponent from './components/ParentComponent';
 import UserGreeting from './components/UserGreeting';
 import NameList from './components/NameList';
+import Stylesheet from './components/Stylesheet';
 function App() {
   return (
     <div className="App">
-      <NameList />
+      <Stylesheet primary = {true} />
+
+    
+      {/* <NameList /> */}
       {/* <UserGreeting /> */}
       {/* <ParentComponent /> */}
       {/* <EventBind /> */}
--- src/components/NameList.js
+++ src/components/NameList.js
@@ -1,26 +1,26 @@
 import React from 'react'
 
 function NameList() {
-    // const name = ['zubair', 'ahmed', 'anas']
-    const x = [
-        {
-            id:1,
-            name: 'zubair',
-            skill: 'react'
+    const name = ['zubair', 'ahmed', 'anas']
+    // const x = [
+    //     {
+    //         id:1,
+    //         name: 'zubair',
+    //         skill: 'react'
 
-        },
-        {
-            id:2,
-            name: 'Ahmed',
-            skill: 'python'
+    //     },
+    //     {
+    //         id:2,
+    //         name: 'Ahmed',
+    //         skill: 'python'
 
-        }  
+    //     }  
 
-    ]
-    const personList = x.map(person =>( 
-        <h1>  Im {person.name}. my skill is{person.skill}</h1>
+    // ]
+    const personList = name.map(person =>( 
+        <h1 key={person}> {name}</h1>
     ))
-    // const namelist = name.map(name => <h1>{name}</h1>)
+    // const namelist = name.map(name => <h1 key>{name}</h1>)
     return (
         <div>
             
