ed9ca9ec6d84e4725949045232683e28a52edb2b
zubairahmedanas <amianasahmed@gmail.com>
2021-10-02 02:35:52 +0600

    Zubair | Inline css added

---
+++ src/components/Inline.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Inline() {
+    const heading ={
+        fontSize: '72px',
+        color:'blue'
+    }
+    return (
+        <div>
+            <h1 style = {heading}>
+                HELLOOO...
+            </h1>
+
+        </div>
+    )
+}
+
+export default Inline
