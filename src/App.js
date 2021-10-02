93f94c5570d5fb327e77c75f9a38617cf4d2d2b6
zubairahmedanas <amianasahmed@gmail.com>
2021-10-02 18:27:48 +0600

    Zubair | css module file added

--- src/App.js
+++ src/App.js
@@ -15,9 +15,13 @@ import UserGreeting from './components/UserGreeting';
 import NameList from './components/NameList';
 import Stylesheet from './components/Stylesheet';
 import Inline from './components/Inline';
+import './appStyles.css'
+import styles from './appStyles.module.css'
 function App() {
   return (
     <div className="App">
+      <h1 className ='error'> Error </h1>
+      <h1 className ={styles.success}> Success </h1>
       <Stylesheet primary = {true} />
       <Inline />    
       {/* <NameList /> */}
